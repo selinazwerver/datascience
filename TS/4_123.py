4f5efbde0681bbac9c890bf0281e4d9b33b36f4d
Selina <selinazwerver@gmail.com>
2020-11-11 20:42:34 +0100

    added 4.3

--- TS/4_123.py
+++ TS/4_123.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import random
 
 print("[Exercise 4.1]")
 
@@ -42,13 +42,13 @@ print()
 
 ### 4.1b: statistics
 print("[4.1b: statistics]")
-nFeatures = 6 # amount of features
+nFeatures = 6  # amount of features
 # Calculate statistics for some features (columns)
 for i in range(0, nFeatures):
-    print(" Feature :", features['feature'][i])
-    print("    Mean : %0.3f" % X[i].mean())
-    print("  Median : %0.3f" % X[i].median())
-    print("  Stddev : %0.3f" % X[i].std())
+    print("Feature :", features['feature'][i])
+    print("   Mean : %0.3f" % X[i].mean())
+    print(" Median : %0.3f" % X[i].median())
+    print(" Stddev : %0.3f" % X[i].std())
     print()
 
 ### Exercise 4.2
@@ -56,19 +56,32 @@ print("[Exercise 4.2]")
 
 ### 4.2a: dimensions
 print("[4.2a: dimensionality]")
-print("Training set :", train_Y.shape)  # (7352, 561)
-print("    Test set :", test_Y.shape)  # (2947, 561)
+print("Training set :", train_Y.shape)  # (7352, 1)
+print("    Test set :", test_Y.shape)  # (2947, 1)
 print()
 
 ### 4.2b: bar chart
 print("[4.2a: bar chart]")
-activity_counts = Y[0].value_counts(normalize=False).sort_index() # count occurrence
+activity_counts = Y[0].value_counts(normalize=False).sort_index()  # count occurrence
 
-# make plot
+# Make plot
 plt.figure(figsize=(15, 6))
 plt.xlabel("Activity")
 plt.ylabel("Counts")
 plt.bar(activity_labels['activity'], activity_counts)
 plt.savefig("figures/4_2b.png", dpi=300)
+print("[4.2a: bar chart saved]")
+print()
 
+### 4.3
+print("[Exercise 4.3]")
+grouped_X = train_X.groupby('label')
+random.seed(9001) # set random number seed
+for i in range(0,10):
+    feature_id = random.randint(0,features.shape[0])
+    plt.clf()
+    grouped_X[feature_id].plot.kde()
+    plt.title(features['feature'].values[feature_id])
+    plt.legend(grouped_X.groups.keys())
+    plt.savefig("figures/4.3_%i.png" % feature_id, dpi = 300)
 
