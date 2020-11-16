f8fb56045611e16205280edf632b01a50e27a2be
Selina <selinazwerver@gmail.com>
2020-11-16 22:22:41 +0100

    working test code for 4.6

--- TS/4_45.py
+++ TS/4_45.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import kurtosis as kurt
+from scipy import fft
 
 print("[Loading data]")
 print()
---
+++ TS/4_6.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy.fftpack import fft
+import matplotlib.pyplot as plt
+
+def calculate_fft(x, t):
+    s =  np.ceil(x.size/2)
+    y = (2/x.size)*fft(x)
+    y = y[0:int(s)]
+    ym = abs(y)
+    f = np.arange(0,s)
+    fspacing = 1/(t.size*(t[1] - t[0]))
+    f = fspacing*f
+    return f, ym
+
+## Test code
+fs = 20 # sample frequency
+T = 1/fs # sample time
+x = np.arange(0, 10, T)
+signal = np.sin(2*2*np.pi*x) + 0.8*np.sin(4*2*np.pi*x) + 0.5*np.sin(6*2*np.pi*x)
+
+f, ym = calculate_fft(signal, x)
+plt.subplot(1,2,1)
+plt.plot(x,signal) # original signal
+plt.subplot(1,2,2)
+plt.plot(f,ym) # fft
+plt.show()
+
+### 4.6
+print("[Exercise 4.6]")
+### 4.6a: fft of all 6 activities
+
+
+
